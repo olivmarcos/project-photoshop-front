e1eae2ef5178585dcf0042dd338feac0e76a4ca7
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2023-09-19 22:42:54 -0300

    styles

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,61 +1,113 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+
+const BASE_URL = 'http://127.0.0.1:5000/api/v1';
 
 function ImageUploader() {
-  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileurl] = useState('');
   const [fileName, setFileName] = useState('');
-  const [uploadedFileUrl, setUploadedFileUrl] = useState('')
-  const [fileUrl, setFileurl] = useState('')
+  const [alteredFileUrl, setAlteredFileUrl] = useState('');
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleOnClick = () => {
+    if (!fileInputRef.current) {
+      return;
+    }
+    fileInputRef?.current.click();
+  }
+
+  const handleSelectedFile = async (e: ChangeEvent<HTMLInputElement>) => {
     const fileList = e.target.files;
 
     if (!fileList) {
-      console.log('ue')
       return;
     }
 
-    const uploadedFile: File = fileList[0];
-    setFile(uploadedFile);
-    setFileName(uploadedFile.name);
-    setFileurl(URL.createObjectURL(uploadedFile))
+    const selectedFile: File = fileList[0];
+    const uploadedFileName = await uploadFile(selectedFile);
+    setFileName(uploadedFileName);
+    setFileurl(`${BASE_URL}/uploads/${uploadedFileName}`);
   }
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const uploadFile = async (file: File) => {
     if (!file) {
       return;
     }
 
     const formData = new FormData();
-    formData.append('file', file, fileName);
-    
+    formData.append('file', file, file.name);
+
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/v1/upload', {
+      const response = await fetch(`${BASE_URL}/upload`, {
         method: 'POST',
         body: formData,
       });
-      const data = await response.json();
-      setUploadedFileUrl(`http://127.0.0.1:5000${data.image_url}`);
-      console.log(uploadedFileUrl);
+
+      const { file_name } = await response.json();
+      return file_name;
     } catch (error) {
       console.error('Error uploading image:', error);
     }
   }
 
+  const handleOnChangeFilter = async (event: ChangeEvent<HTMLSelectElement>) => {
+    const filterToApply = event.target.value;
+    if (!filterToApply) {
+      return;
+    }
+    const alteredFileName = await applyFilter(filterToApply);
+    setAlteredFileUrl(`${BASE_URL}/altered/${alteredFileName}`);
+  }
+
+  const applyFilter = async (filterToApply: string) => {
+    const body = {
+      filterToApply,
+      fileName
+    }
+
+    try {
+      const response = await fetch(`${BASE_URL}/filter`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      });
+      const { file_name } = await response.json();
+      return file_name;
+    } catch (error) {
+      console.error('Erro applying filter', error);
+    }
+  };
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <br />
-        <br />
-        <button type="submit">Upload Image</button>
-      </form>
+      <div className="">
+        <div className="flex gap-4">
+          <select className="py-3 px-10" name="filters" id="filters" defaultValue={'default'} disabled={!fileUrl} onChange={handleOnChangeFilter}>
+            <option value="default">Filtros</option>
+            <option value="negative">Negativo</option>
+          </select>
+        </div>
 
-      <div>
-      {fileUrl && (
-        <img src={fileUrl} alt="Uploaded" />
-      )}
+        <div className="flex items-center justify-center gap-4">
+          <div className="p-4 shadow-2xl w-[500px] h-[600px] flex items-center justify-center">
+            {!fileUrl && (
+              <button className="py-3 px-2 bg-black text-white rounded-md" onClick={handleOnClick}>Selecione uma imagem</button>
+            )}
+            {fileUrl && (
+              <img className="w-full h-full" src={fileUrl} alt="originalImage" />
+            )}
+          </div>
+
+          <div className="w-[500px] h-[600px] p-4 shadow-2xl">
+            {alteredFileUrl && (
+              <img className="w-full h-full" src={alteredFileUrl} alt="filteredImage" />
+            )}
+          </div>
+        </div>
       </div>
+
+      <input ref={fileInputRef} type="file" accept=".jpg, .jpeg, .png, .bmp" onChange={handleSelectedFile} style={{ display: "none" }} />
     </>
   );
 }
