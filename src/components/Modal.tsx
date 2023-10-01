06999d74334eb51f34d4812c4012a758a4fb0cee
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2023-10-01 15:15:26 -0300

    refact components

---
+++ src/components/Modal.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+
+interface ModalProps {
+  children?: ReactNode
+  isOpen: boolean,
+  onClose: () => void;
+}
+
+function Modal({ isOpen, onClose, children }: ModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div 
+      className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-lg shadow-lg w-5/6 h-5/6 overflow-auto flex items-center justify-center">
+        <button
+          className="absolute top-12 right-8 text-white hover:text-gray-700"
+          onClick={onClose}
+        >
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -2,8 +2,11 @@ import ImageUploader from "../components/ImageUploader";
 
 function Editor() {
   return (
-    <div>
-      <h1 className="mb-10">Editor</h1>
+    <div className="w-full h-full flex flex-col items-center gap-4">
+      <div className="bg-rose-400 text-white flex items-center justify-between w-full h-16 px-6 rounded-md shadow-md">
+        <span className="font-bold">PDI - Project Photoshop</span>
+        {/* <span>Download images</span> */}
+      </div>
       <ImageUploader></ImageUploader>
     </div>
   );
