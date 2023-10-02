aa0041868792776125043d7936e07131ced21c96
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2023-10-01 21:09:10 -0300

    add histogram button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ function Modal({ isOpen, onClose, children }: ModalProps) {
       className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
       <div className="bg-white rounded-lg shadow-lg w-5/6 h-5/6 overflow-auto flex items-center justify-center">
         <button
-          className="absolute top-12 right-8 text-white hover:text-gray-700"
+          className="absolute top-11 right-32 text-white hover:text-gray-700"
           onClick={onClose}
         >
           X
